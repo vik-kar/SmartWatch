9fa56d83ef1977126e9c2e2c2b39fd719d2a4202
Vikram Karmarkar <vikrampk2003@gmail.com>
2025-06-26 16:04:59 -0700

    initial burst command function - now, modify display_init() to use it

--- main/i2c_display.c
+++ main/i2c_display.c
@@ -47,6 +47,24 @@ void send_command(uint8_t cmd){
 	i2c_cmd_link_delete(command);
 }
 
+void send_command_burst(const uint8_t *commands, size_t len){
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (DISPLAY_ADDR << 1) | I2C_MASTER_WRITE, true);
+
+	for(size_t i = 0; i < len; i++){
+		/* control byte: indicate that one command is coming */
+		i2c_master_write_byte(cmd, DISPLAY_ONE_CMD, true);
+
+		/* write the actual command */
+		i2c_master_write_byte(cmd, commands[i], true);
+	}
+	i2c_master_stop(cmd);
+	i2c_master_cmd_begin(I2C_PORT, cmd, 100 / portTICK_PERIOD_MS);
+	i2c_cmd_link_delete(cmd);
+}
+
 void send_data(uint8_t data){
 	i2c_cmd_handle_t command = i2c_cmd_link_create();
 	i2c_master_start(command);
