63f9fb2818e807ab0a68f775f04af4c6020c60d9
Vikram Karmarkar <vikrampk2003@gmail.com>
2025-06-27 19:48:19 -0700

    adxl345 successfully initialized, next: display x, y, and z position data on screen, then move on to synchronization

--- main/adxl.h
+++ main/adxl.h
@@ -2,6 +2,7 @@
 #define ADXL_H_
 
 #include "i2c_display.h"
+#include "esp_err.h"
 #include <stdint.h>
 
 #define DEVID_R			0x00
@@ -11,7 +12,7 @@
 #define DATA_START_ADDR	0x32
 
 #define MEASURE_EN			(1U << 3)
-#define DATA_FORMAT_CONF	((1U << 3) | 1U << 0))
+#define DATA_FORMAT_CONF	((1U << 3) | (1U << 0))
 
 esp_err_t adxl_init();
 void adxl_read_values (uint8_t reg);
--- main/main.c
+++ main/main.c
@@ -13,23 +13,32 @@ void app_main() {
     display_init_burst();
     clear_display();
 
-    esp_err_t read_result;
-    uint8_t dev_id_val;
-    read_result = i2c_read_register(DEVICE_ADDR, DEVID_R, &dev_id_val , 1);
+//    esp_err_t read_result;
+//    uint8_t dev_id_val;
+//    read_result = i2c_read_register(DEVICE_ADDR, DEVID_R, &dev_id_val , 1);
+
+    esp_err_t err = adxl_init();
+    if(err == ESP_OK){
+		esp_err_t read_result;
+		uint8_t dev_id_val;
+		read_result = i2c_read_register(DEVICE_ADDR, DEVID_R, &dev_id_val , 1);
+
+		char buffer[4];
+		sprintf(buffer, "%02X", dev_id_val);
+		display_burst_write_string(buffer, 0, 2);
+		display_burst_write_string("hello", 0, 5);
+    }
 
     vTaskDelay(pdMS_TO_TICKS(1000));
 
-    char buffer[4];
-    sprintf(buffer, "%02X", dev_id_val);
-    display_burst_write_string(buffer, 0, 2);
+//    char buffer[4];
+//    sprintf(buffer, "%02X", dev_id_val);
+//    display_burst_write_string(buffer, 0, 2);
 
 
     //esp_task_wdt_init(&config);
     //esp_task_wdt_add(NULL);
 
-
-
-    vTaskDelay(pdMS_TO_TICKS(1000));
     //display_burst_write_string("A long time ago, in a galaxy far, far away...", 0, 0);
     //display_write_char('A', 0, 0);
     /* infinite loop to prevent watchdog reset - main task keeps running */
