f32e89e04e8788484ad25a0e4f8847a4004950cc
Vikram Karmarkar <vikrampk2003@gmail.com>
2025-06-30 17:54:27 -0700

    wifi_init() implemented. need to define callbacks for when wifi is connected and IP is obtained. Need WiFi for SNTP time sync to display the current time at top left.

---
+++ main/wifi_connect.c
@@ -0,0 +1,95 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "wifi_connect.h"
+#include "freertos/event_groups.h"
+
+#define TAG "WiFi"
+
+/* WPA2-PSK is common for home networks */
+#define WIFI_AUTHMODE WIFI_AUTH_WPA2_PSK
+
+/* Event group bits */
+#define WIFI_CONNECTED_BIT 	BIT0	// connection success
+#define WIFI_FAIL_BIT      	BIT1	// connection failure
+
+/* Retry configuration */
+static const int WIFI_RETRY_ATTEMPT = 3;
+static int 		 wifi_retry_count	= 0;
+
+/* WiFi related handles
+
+ * `esp_netif_t` is a handle representing a network interface. It is part of the ESP-IDF's abstraction
+   that manages IP config, routing, DNS, callbacks, etc
+
+*/
+static esp_netif_t *wifi_netif = NULL;
+static esp_event_handler_instance_t ip_event_handler;
+static esp_event_handler_instance_t wifi_event_handler;
+
+/* Event group handle */
+static EventGroupHandle_t wifi_event_group = NULL;
+
+esp_err_t wifi_init(void){
+	/* Declare a return value for ESP-IDF function calls */
+	esp_err_t ret;
+	/* Initialize NVS for credential storage */
+	ret = nvs_flash_init();
+
+	if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND){
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+
+	/* Create an event group so that we can use the bits defined above to track events */
+	wifi_event_group = xEventGroupCreate();
+
+	/* Initialize the entire netif system, so that we can declare specific netif's later */
+	ret = esp_netif_init();
+
+	if(ret != ESP_OK){
+		ESP_LOGI(TAG, "ERROR: Could not initialize TCP/IP Network Stack.");
+		return ret;
+	}
+
+	/* Create a global event loop, which collects and dispatches system level events, like WIFI_EVENT and IP_EVENT */
+	ret = esp_event_loop_create_default();
+	if (ret != ESP_OK) {
+		ESP_LOGI(TAG, "ERROR: Could not create default event loop.");
+		return ret;
+	}
+
+	ret = esp_wifi_set_default_wifi_sta_handlers();
+	if (ret != ESP_OK) {
+		ESP_LOGI(TAG, "ERROR: Could not create default event loop");
+		return ret;
+	}
+
+	wifi_netif = esp_netif_create_default_wifi_sta();
+	if(wifi_netif == NULL){
+		ESP_LOGI(TAG, "ERROR: Could not create default WiFi STA (station mode) interface");
+		return ESP_FAIL;
+	}
+
+	/* Now, configure the parameters for the WiFi stack */
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+														ESP_EVENT_ANY_ID,
+														&wifi_event_cb,
+														NULL,
+														&wifi_event_handler));
+
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+														ESP_EVENT_ANY_ID,
+														&ip_event_cb,
+														NULL,
+														&ip_event_handler));
+
+}
+
+
+
+
